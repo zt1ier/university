8072d4a7f40b4f88e02e89ea3b58008e392fdca5
Fritz <fglemeric@gmail.com>
2025-08-20 08:32:16 +0800

    Create print-letter.c

---
+++ first-year/c/lab-work/print-letter.c
@@ -0,0 +1,15 @@
+// August 20, 2025
+
+#include <stdio.h>
+
+int main() {
+    char char1, char2;
+    
+    char1 = 'F';
+    char2 = 'f';
+    
+    printf("First character: %c\n", char1);
+    printf("Second character: %c", char2);
+
+    return 0;
+}
