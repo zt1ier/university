05d579f410c743e7546b920ca9f139679ab8dfff
Fritz <fglemeric@gmail.com>
2025-08-20 09:13:29 +0800

    Create print-vars.c

---
+++ first-year/c/lab-work/print-vars.c
@@ -0,0 +1,27 @@
+// August 20, 2025
+
+#include <stdio.h>
+
+
+int main() {
+    int x, y, z;
+    char a, b, c;
+    
+    x = 100;
+    y = 200;
+    z = (x + y) / 2;
+    
+    a = 'P';
+    b = 'X';
+    c = 'z';
+    
+    printf("Int x: %d\n", x);
+    printf("Int y: %d\n", y);
+    printf("Int z: %d\n", z);
+    
+    printf("Char a: %c\n", a);
+    printf("Char b: %c\n", b);
+    printf("Char c: %c", c);
+
+    return 0;
+}
