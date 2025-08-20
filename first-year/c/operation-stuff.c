1dd972513ee7409814d08c4ad28a8af22f9adeb4
Fritz <fglemeric@gmail.com>
2025-08-20 08:30:48 +0800

    Create operation-stuff.c

---
+++ first-year/c/operation-stuff.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+
+int main() {
+    float num1;
+    float num2;
+    char operation;
+    float result;
+    
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    printf("You entered: %.2f\n", num1);
+    
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+    printf("You entered: %.2f\n", num2);
+    
+    printf("Enter an operation (+, -, *, /): ");
+    scanf("%s", &operation);
+    printf("Operation entered: %c\n", operation);
+
+    
+    switch(operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+            
+        case '-':
+            result = num1 - num2;
+            break;
+            
+        case '*':
+            result = num1 * num2;
+            break;
+            
+        case '/':
+        result = num1 / num2;
+            break;
+            
+        default:
+            printf("Invalid operation");
+            break;
+    }
+    
+    printf("Result is: %.2f", result);
+
+    return 0;
+}
