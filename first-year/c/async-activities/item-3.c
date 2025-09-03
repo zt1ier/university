df363b8d0fbf352327b8ad83b22ea9069e5a6bc4
Fritz <fglemeric@gmail.com>
2025-09-03 10:39:29 +0800

    Create item-3.c

---
+++ first-year/c/async-activities/item-3.c
@@ -0,0 +1,29 @@
+// September 03, 2025
+
+#include <stdio.h>
+
+
+int main() {
+
+    // input liters and miles travelled
+    // convert liters -> gallons; get miles travelled per gallon (miles divided by gallons)
+    // output miles per gallon car delivered
+
+    float literToGallons = 0.264179; // 1 liter = 0.264179 gallons
+    float litersConsumedByCar, milesTravelled; // input
+    float gallons, milesPerGallon; // output
+
+    printf("\nEnter liters consumed by car: ");
+    scanf("%f", &litersConsumedByCar);
+
+    printf("\nEnter miles travelled: ");
+    scanf("%f", &milesTravelled);
+
+    gallons =  litersConsumedByCar * literToGallons;
+    
+    milesPerGallon = milesTravelled / gallons;
+
+    printf("\nMiles per gallon the car delivered: %.2f\n", milesPerGallon);
+
+    return 0;
+}
