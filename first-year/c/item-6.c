09b60987b4677672fc5ff25520c06a662396ea52
Fritz <fglemeric@gmail.com>
2025-09-04 08:03:11 +0800

    Create item-6.c

---
+++ first-year/c/item-6.c
@@ -0,0 +1,36 @@
+// September 4, 2025
+
+#include <stdio.h>
+
+
+int main() {
+
+    // input: beginning balance
+    // output: balance at the end of the year
+    // interest of 3.5% compounded quarterly
+
+    const float interestRate = 0.035; // 3.5%
+    const int compoundingPeriods = 4; // quarterly = 4
+
+    float beginningBalance, previousBalance, currentBalance, endingBalance;
+    float quarterlyInterest;
+
+    // input
+    printf("\nCurrent bank balance: ");
+    scanf("%f", &beginningBalance);
+
+    // math
+    quarterlyInterest = interestRate / compoundingPeriods;
+
+    endingBalance = beginningBalance + beginningBalance * quarterlyInterest;    // quarter 1
+    endingBalance = endingBalance + endingBalance * quarterlyInterest;          // quarter 2
+    endingBalance = endingBalance + endingBalance * quarterlyInterest;          // quarter 3
+    endingBalance = endingBalance + endingBalance * quarterlyInterest;          // quarter 4
+    // no for-loop, hardcoding hurts :(
+
+    // output
+    printf("\nBalance at the end of 1 year with a 3.5%% quarterly compound interest: %.2f", 
+        endingBalance);
+
+    return 0;
+}
